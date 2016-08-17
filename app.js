261fe0a6c93f3a78b71f3018d7de66c3e3fc9dc6
Thomas Roberson <thomas_roberson@icloud.com>
2016-08-17 21:31:28 +0800

    added analyser boilerplate

--- app.js
+++ app.js
@@ -97,13 +97,17 @@ function modulation(car, mod, typeofmod) {
 
 let src;
 let startBtn = document.getElementById("start");
+let analyser = audioContext.createAnalyser();
 
 startBtn.addEventListener("click", () => {
+    draw();
     src = modulation(createOsc("sawtooth", 220, 0.3),
                      createOsc("sine", frequency, amplitude),
                      toggle);
     
     src.car.osc.start(audioContext.currenTime);
+    src.car.connect(analyser);
+    analyser.connect(audioContext.destination);
     src.mod.osc.start(audioContext.currentTIme);
 });
 
@@ -113,6 +117,47 @@ stopBtn.addEventListener("click", () => {
     src.car.osc.stop(audioContext.currenTime);
     src.mod.osc.stop(audioContext.currentTIme);
 });
+
+var canvas = document.querySelector('.visualizer');
+var myCanvas = canvas.getContext("2d");
+var WIDTH = 300;
+var HEIGHT = 300;
+
+myCanvas.clearRect(0, 0, WIDTH, HEIGHT);
+var bufferLength = analyser.frequencyBinCount;
+var dataArray = new Uint8Array(bufferLength);
+
+function draw() {
+    let drawVisual = requestAnimationFrame(draw);
+    analyser.getByteTimeDomainData(dataArray);
+    
+    myCanvas.fillStyle = 'rgb(200, 200, 200)';
+    myCanvas.fillRect(0, 0, WIDTH, HEIGHT);
+    myCanvas.lineWidth = 2;
+    myCanvas.strokeStyle = 'rgb(0, 0, 0)';
+
+    myCanvas.beginPath();
+    var sliceWidth = WIDTH * 1.0 / bufferLength;
+    var x = 0;
+    
+    for(var i = 0; i < bufferLength; i++) {
+        
+        var v = dataArray[i] / 128.0;
+        var y = v * HEIGHT/2;
+
+        if(i === 0) {
+            myCanvas.moveTo(x, y);
+        } else {
+            myCanvas.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+    };
+    
+    myCanvas.lineTo(canvas.width, canvas.height/2);
+    myCanvas.stroke();
+};
+
 
 
 
