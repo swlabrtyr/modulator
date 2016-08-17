b4dd0e08c11e2153798c7e5af9c792305bf6abc4
Thomas Roberson <thomas_roberson@icloud.com>
2016-08-17 20:02:05 +0800

    need to fix UI logic

--- app.js
+++ app.js
@@ -38,10 +38,7 @@ function createOsc(type, freq, amp) {
     };
 }
 
-let frequency, amplitude;
-
-
-let toggle;
+let toggle = "FM";
 
 let amInput = document.getElementById("am-on");
 let fmInput = document.getElementById("fm-on");
@@ -54,73 +51,74 @@ fmInput.addEventListener("change", function() {
     toggle = "FM";
 });
 
-function modulation(freq, amnt, typeofmod) {
-    console.log("modulation: ", freq, amnt);
+let frequency = 0, amplitude = 0;
+
+function modulation(typeofmod) {
 
     let carrier, mod;
 
-    let freqInput = document.getElementById("freq");
-    let ampInput  = document.getElementById("amp");
+    carrier = createOsc("sawtooth", 220, 0.5);
+    carrier.connect();
     
-    function getFreq() {
-        return freqInput.value;
-        console.log(freqInput.value);
+    mod = createOsc("sine", frequency, amplitude);
+    mod.connect();        
+    
+    if (typeofmod === "FM") {
+
+        mod.gain.connect(carrier.osc.frequency);
+        
+    } else if (typeofmod === "AM") {
+        
+        mod.gain.connect(carrier.gain.gain);
+        
     }
 
-    function getAmp() {
-        return ampInput.value;
-        console.log(ampInput.value);
-    }
-
-    freqInput.addEventListener("input", () => {
-        frequency = parseInt(getFreq());
-
-        console.log(frequency, amplitude);
-    });
-
-    ampInput.addEventListener("input", () => {
-        amplitude = parseInt(getAmp());
-
-        console.log(frequency, amplitude);
-    });
-
-    let startBtn = document.getElementById("start");
-
-    startBtn.addEventListener("click", () => {
-
-        carrier = createOsc("sawtooth", 220, 0.5);
-        carrier.connect();
-        
-        mod = createOsc("sine", freq, amnt);
-        mod.connect();        
-       
-        if (typeofmod === "FM") {
-
-            mod.gain.connect(carrier.osc.frequency);
-            
-        } else if (typeofmod === "AM") {
-            
-            mod.gain.connect(carrier.gain.gain);
-            
-        }
-
-        carrier.connectToOutput();
-        
-        mod.osc.start(audioContext.currenTime);
-        carrier.osc.start(audioContext.currentTIme);
-    });
-
-    let stopBtn = document.getElementById("stop");
-
-    stopBtn.addEventListener("click", () => {
-
-        mod.osc.stop(audioContext.currenTime);
-        carrier.osc.stop(audioContext.currentTime);
-    });
+    carrier.connectToOutput();
 }
 
+let freqInput = document.getElementById("freq");
+let ampInput  = document.getElementById("amp");
+
+function getFreq() {
+    return freqInput.value;
+    console.log(freqInput.value);
+}
+
+function getAmp() {
+    return ampInput.value;
+    console.log(ampInput.value);
+}
+
+freqInput.addEventListener("input", () => {
+    frequency = parseInt(getFreq());
+
+    console.log(frequency, amplitude);
+});
+
+ampInput.addEventListener("input", () => {
+    amplitude = parseInt(getAmp());
+
+    console.log(frequency, amplitude);
+});
+
+
+let startBtn = document.getElementById("start");
+
+startBtn.addEventListener("click", () => {
+    mod.osc.start(audioContext.currenTime);
+    carrier.osc.start(audioContext.currentTIme);
+});
+
+let stopBtn = document.getElementById("stop");
+
+stopBtn.addEventListener("click", () => {
+
+    mod.osc.stop(audioContext.currenTime);
+    carrier.osc.stop(audioContext.currentTime);
+});
+
 // BE WARY OF INPUT VALUES IN CASE OF AM
-modulation(frequency, amplitude, "FM");
+modulation(toggle);
 
 
 
